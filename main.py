4e44c3be0f1e9a27b3447d3d7ba0bedda9a504e2
Andrew Gein <andgein@yandex.ru>
2017-11-13 23:18:55 +0500

    Запуск тестов конкретного решателя

--- main.py
+++ main.py
@@ -8,10 +8,22 @@ TOKEN = '745ba685-9cae-43ef-b1e6-2dbaf662b9c6'
 
 
 if __name__ == '__main__':
-    solvers = [colors.Solver(), accentuation.Solver(), tts.Solver(), aplusb.Solver()]
+    solvers = [
+        colors.Solver(),
+        accentuation.Solver(),
+        tts.Solver(),
+        aplusb.Solver(),
+        time.Solver(),
+    ]
 
-    if len(sys.argv) > 1 and sys.argv[1] == 'test':        
+    if len(sys.argv) > 1 and sys.argv[1] == 'test':
+        module_name = None
+        if len(sys.argv) > 2:
+            module_name = sys.argv[2]
+
         for solver in solvers:
+            if module_name is not None and type(solver).__module__ != 'solvers.' + module_name:
+                continue            
             solver.run_tests()
         sys.exit(0)
     
